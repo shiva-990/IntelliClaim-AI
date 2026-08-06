fc92b455e56758ea9d271ab89bf844f21a058b82
Anand <anand15apr2004@gmail.com>
2026-08-07 00:52:13 +0530

    Add Groq settings

--- configs/settings.py
+++ configs/settings.py
@@ -28,6 +28,7 @@ class Settings(BaseSettings):
     PINECONE_INDEX_NAME: str = ""
 
     GROQ_API_KEY: str = ""
+    GROQ_MODEL: str = "llama-3.1-8b-instant"
 
     SECRET_KEY: str = "change-this"
 
